63b57a80999ea1c21664eb1ad0276a3f0555e6d5
fifsky <fifsky@gmail.com>
2021-02-01 15:35:43 +0800

    feat: update go mod

--- testutil/init.go
+++ testutil/init.go
@@ -1,14 +1,12 @@
 package testutil
 
 import (
-	"github.com/goapt/golib/debug"
 	"github.com/goapt/logger"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func init() {
-	debug.Open("on", "")
 	logger.Setting(func(c *logger.Config) {
 		c.LogMode = "std"
 	})
